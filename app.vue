f3e485fa8858dea766bb1bd79df4edbed2fb70a6
Liliane Basílio <liliane.basilio@softagon.com.br>
2023-06-22 17:07:09 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+  <h1>Olá extends bora funcionar?</h1>
+  <Saudacao/>
+  <Form/>
+  <v-btn class="bg-pink-darken-4 ma-16">ok</v-btn>
+  </div>
+</template>
+<script setup>
+
+</script>
